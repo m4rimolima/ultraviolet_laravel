bd6318d4de43ac71ce9472a763bf01c41985fb9a
m4rimolima <m4rimolima@gmail.com>
2025-09-28 18:42:14 -0300

    lista do index com html

--- librarySystem/resources/views/categories/index.blade.php
+++ librarySystem/resources/views/categories/index.blade.php
@@ -1,5 +1,15 @@
-<ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <ul>
     @foreach ($categories as $category)
     <li>{{$category -> name}}</li>
     @endforeach
-</ul>
+</ul>   
+</body>
+</html>
