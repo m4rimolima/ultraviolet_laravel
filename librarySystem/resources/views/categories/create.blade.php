50f3791cb9df0e2424f8c68d51267292f467f6d7
m4rimolima <m4rimolima@gmail.com>
2025-09-28 18:40:21 -0300

    form com csrf

--- librarySystem/resources/views/categories/create.blade.php
+++ librarySystem/resources/views/categories/create.blade.php
@@ -1,5 +1,5 @@
 <form action="{{route('categories.store')}" method="post">
-
+    @csrf
     <label for="name">Category name: </label>
     <input type="text" name="name" id="name">
     <input type="submit" value="Create Category">
