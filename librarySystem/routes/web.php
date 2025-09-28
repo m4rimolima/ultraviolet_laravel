6df72eb2aac593e1c4482db66650263956c4468e
m4rimolima <m4rimolima@gmail.com>
2025-09-28 18:35:12 -0300

    rota do store

---
+++ librarySystem/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryControllerController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route:: post('/categories',[CategoryController::class, 'store'])->name('categories.store');
